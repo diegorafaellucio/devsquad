74e2fdec373ba34ec677d271b80d2a27822910fc
Diego Rafael Lucio <diegorafaellucio@gmail.com>
2018-08-12 16:49:10 -0300

    Import

--- app/Console/Commands/importproducts.php
+++ app/Console/Commands/importproducts.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Collection;
 
 class importproducts extends Command
 {
@@ -38,6 +41,25 @@ class importproducts extends Command
     public function handle()
     {
         //
-        Storage::disk('local')->files('optional_dir_name');
+        $files = DB::table('files')
+            ->select('*')
+            ->where('processed', '=', '0')
+            ->get()->toArray();
+
+        $mainPath = Storage::disk('public')->getAdapter()->getPathPrefix();
+
+        foreach ($files as $file){
+            $fileName =  "importProducts/{$file->name}";
+            $filePath = "{$mainPath}{$fileName}";
+
+            Excel::load($filePath)->each(function (Collection $csvLine) {
+
+                Character::create([
+                    'name' => "{$csvLine->get('first_name')} {$csvLine->get('last_name')}",
+                    'job' => $csvLine->get('job'),
+                ]);
+
+            });
+        }
     }
 }
